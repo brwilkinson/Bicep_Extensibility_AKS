18353798b073327dc7c9f0c1fb64885d7f6b5e99
brwilkinson <brwilkinson@outlook.com>
2023-04-01 15:45:49 -0700

    update main

--- setup_deploy.ps1
+++ setup_deploy.ps1
@@ -7,7 +7,7 @@ Write-Output 'requires restart'
 $splat = @{
     Name                  = 'Namespace_Bicep'
     ResourceGroupName     = 'AEU1-PE-CTL-RG-D1'
-    TemplateFile          = 'D:\repos\Namespace_Bicep\bicep\default.bicep'
+    TemplateFile          = 'D:\repos\Namespace_Bicep\bicep\main.bicep'
     # bicepparam compilation not supported as yet
     # TemplateParameterFile = 'D:\repos\Namespace_Bicep\namespaces\LAB\values-d1.bicepparam'
 }

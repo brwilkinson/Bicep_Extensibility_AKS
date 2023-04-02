f0409bfb56a0ebc3054f2641e0e45f60d1b3bcb2
brwilkinson <brwilkinson@outlook.com>
2023-04-01 18:16:04 -0700

    update service name

--- setup_deploy.ps1
+++ setup_deploy.ps1
@@ -9,7 +9,7 @@ $splat = @{
     ResourceGroupName     = 'AEU1-PE-CTL-RG-D1'
     TemplateFile          = 'D:\repos\Namespace_Bicep\bicep\main.bicep'
     # bicepparam compilation not supported as yet
-    # TemplateParameterFile = 'D:\repos\Namespace_Bicep\namespaces\LAB\values-d1.bicepparam'
+    # TemplateParameterFile = 'D:\repos\Namespace_Bicep\tenants\LAB\values-d1.bicepparam'
 }
 
 New-AzResourceGroupDeployment @splat -Verbose
